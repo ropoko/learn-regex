6c97e6390a4cd0268ad037791bec6429a697174a
ropoko <rodrigostramantinoli@gmail.com>
2021-11-11 00:22:50 -0300

    feat: regex option for numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
+  <div class="container">
+    <header>
       <h1>
-        Welcome to {{title}}!
+        Hello world!
       </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+    </header>
+    <main>
+      <form [formGroup]="form">
+        <label for="numbers">
+          <input (change)="regex()" formControlName="numbers" type="checkbox" id="numbers">
+          Get numbers
+        </label>
+
+        <section formGroupName="inputs">
+          <label for="reponse">Test text goes here</label>
+          <textarea (keydown)="regex()" (focus)="regex()" formControlName="testText" id="response" cols="45" rows="10"></textarea>
+          <input formControlName="matchedString" class="matchText" value="{{matchString}}" readonly />
+        </section>
+      </form>
+    </main>
+  </div>
   `,
-  styles: []
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+  matchString: string = '';
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      numbers: [false],
+      inputs: this.fb.group({
+        testText: [' auhduhs 123123'],
+        matchedString: ['']
+      })
+    });
+  }
+
+  regex(): void {
+    const testText: AbstractControl = this.form.get('inputs.testText')!;
+
+    Object.keys(this.form.controls).forEach(key => {
+      if (key !== 'inputs') {
+        // apply regex to numbers
+        if (this.form.get('numbers')?.value === true) {
+          this.matchString = testText.value.match(/[0-9]/gm)?.join('');
+        } else {
+          this.form.get('inputs.matchedString')?.patchValue('');
+        }
+      }
+    });
+  }
 }
